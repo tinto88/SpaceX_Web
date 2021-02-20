d76b04fb596eb6862a8b377cd9bde78a38ca9af7
sspim145 <69472050+sspim145@users.noreply.github.com>
2021-02-21 06:33:02 +0700

    edit-rocket

--- spacex/src/pages/Rockets/Rockets.js
+++ spacex/src/pages/Rockets/Rockets.js
@@ -17,7 +17,11 @@ function Rockets() {
   return (
     <>
       <div className="container-rocket">
-        
+        <img className="bg-cont-rockets" 
+        src="https://images.unsplash.com/photo-1567416661576-659c4298a2c6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80"></img>
+        <div className="center-rockets">
+        <h1>ROCKETS</h1>
+        </div>
       </div>
       <div className="container">
         {/* <div className="card-container">
