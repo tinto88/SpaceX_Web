6e01f7deaba8171fa909fb0db55b87d186799fb1
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2021-02-22 04:40:57 +0700

    Update RocketDetail.js

--- spacex/src/pages/RocketDetail/RocketDetail.js
+++ spacex/src/pages/RocketDetail/RocketDetail.js
@@ -1,6 +1,6 @@
 import React, { Component, useState, useEffect, version } from "react";
 import "./RocketDetail.css";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 
 function RocketDetail() {
   const { rocketId } = useParams();
@@ -23,19 +23,26 @@ function RocketDetail() {
           {/* <h1 >RocketDetail</h1> */}
           <div className="container-r-detail-1">
             <div className="content-container1-detail">
-                <h1>{rockets.rocket_name}</h1>
+              <h1>{rockets.rocket_name}</h1>
               <div className="content-engine">
                 <p>First Flight : {rockets.first_flight}</p>
                 <p>Country : {rockets.country}</p>
-                <p>Height : {rockets.height.lb} lb / Mass : {rockets.mass.kg} kg</p>
+                <p>
+                  Height : {rockets.height.lb} lb / Mass : {rockets.mass.kg} kg
+                </p>
                 <p>Stages : {rockets.stages}</p>
                 <h2>Engines - {rockets.engines.type}</h2>
-                <p>Propellant : {rockets.engines.propellant_1}, {rockets.engines.propellant_2}</p>
+                <p>
+                  Propellant : {rockets.engines.propellant_1},{" "}
+                  {rockets.engines.propellant_2}
+                </p>
               </div>
               <div className="content-thrust">
                 <div className="detail-content-thrust">
                   <h4></h4>
-                  <p>Thrust Sea Level : {rockets.engines.thrust_sea_level.lbf}</p>
+                  <p>
+                    Thrust Sea Level : {rockets.engines.thrust_sea_level.lbf}
+                  </p>
                 </div>
                 <div className="detail-content-thrust">
                   <p>Thrust Vacuum : {rockets.engines.thrust_vacuum.lbf}</p>
@@ -43,10 +50,13 @@ function RocketDetail() {
                 <div className="detail-content-thrust">
                   <p>Thrust To Weight : {rockets.engines.thrust_to_weight}</p>
                 </div>
-                
               </div>
             </div>
-            <div className="back-button"><h2>SSPIM</h2></div>
+            <div className="back-button">
+              <NavLink className="btnTest button-primary" to={"/rockets"}>
+                Back
+              </NavLink>
+            </div>
           </div>
           <div className="container-r-detail-2">
             <img className="img-r-detail" src={rockets.flickr_images[0]} />
